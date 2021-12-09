deefcab8a873455b1e274b8e384f5756e21e4f96
adanzan <danzanachiterdene@gmail.com>
2021-12-09 13:38:53 -0500

    Setup of Netlogo

--- Driver.py
+++ Driver.py
@@ -1,20 +1,23 @@
 import os
 from Graph_MST_library import *
-  
-# PLEASE CHANGE THIS PATH TO WHERE THE TEST FIELS ARE STORED IN YOUR COMPUTER
-path = r"C:\Users\Majd\Desktop\Middlebury\1- Midd Cources\4- Senior\1- Fall\CSCI 302\HWs\IPA-2\CS302_project_group\MST-Test-Files"
+
+# PLEASE CHANGE THIS PATH TO WHERE THE TEST FILES ARE STORED IN YOUR COMPUTER
+#path = r"C:\Users\Majd\Desktop\Middlebury\1- Midd Cources\4- Senior\1- Fall\CSCI 302\HWs\IPA-2\CS302_project_group\MST-Test-Files"
+path = "/Users/Danzan/workspace/CS-Middlebury/CS302_MST_/MST-Test-Files"
+
 
 os.chdir(path)
-  
+
 files = []
 for file in os.listdir():
 #     print(str(file).split(".txt"))
     if file.endswith(".txt"):
-        file_path = f"{path}\{file}"
+        #file_path = f"{path}\{file}"
+        file_path = f"{path}/{file}"
         f = open(file_path, 'r')
         files.append(f.readlines())
         f.close()
-        
+
 
 for file in files:
     num_lines = len(file)
@@ -22,11 +25,11 @@ for file in files:
     g = file[0].split()
     n = int(g[0])
     m = int(g[1])
-    
+
     # create a graph
     g = Graph_kruskal(n, m)
     g_2 = Graph_prim(n, m)
-    
+
     # fill graph
     for i in range(1, num_lines-1):
         line = file[i].split()
@@ -35,13 +38,9 @@ for file in files:
         w = float(line[2])
         g.add_edge([u, v, w])
         g_2.add_edge([u, v, w])
-    
+
     # run kruskal
     cost, MST = g.kruskal()
     print("Kruskal's method MST cost = " + str(cost) + " known cost = " + str(file[num_lines-1]))
     cost, MST = g_2.prim()
     print("Prim's method MST cost = " + str(cost) + " known cost = " + str(file[num_lines-1]))
-        
-        
-    
-    
